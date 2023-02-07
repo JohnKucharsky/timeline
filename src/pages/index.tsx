9c6e45bdd7485c32820619d92254bb8e475cfdf5
John <vannok@live.com>
2023-02-07 14:58:05 +0300

    timeline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import styles from '@/styles/Home.module.css'
+import {useRef, useState} from "react";
 
 export default function Home() {
+    const [position,setPosition] = useState(0)
+    const [isMoving,setIsMoving] = useState(false)
+
+    const ref = useRef<any>(null)
+    const width = ref.current?.clientWidth
+
+    const onPointerDown = (e:any)=>{
+        ref.current.setPointerCapture(e.pointerId)
+        setIsMoving(true)
+        setPosition(e.clientX/width*100)
+
+    }
+
+    const onPointerMove=(e:any)=>{
+        if(!isMoving)return
+        setPosition(e.clientX/width*100)
+    }
+    const onPointerUp=()=>{
+        setIsMoving(false)
+
+    }
+
   return (
     <>
       <Head>
@@ -15,108 +35,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+            <div className={styles.buttons}>
+                <button onClick={()=>setPosition(p=>p+5)}>+</button>
+                <button onClick={()=>setPosition(p=>p-5)}>-</button>
+            </div>
+          <div onPointerUp={()=>onPointerUp()} onPointerMove={(e)=>onPointerMove(e)} ref={ref} onPointerDown={(e)=>onPointerDown(e)} className={styles.timeline}>
+              <div style={{
+                  left:`${position}%`
+              }} className={styles.dot}></div>
+              <div style={{
+                  width: `${position+3}%`
+              }} className={styles.timeline_filled}></div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   )
